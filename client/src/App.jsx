a3ae641dfeeae7daeb73d02d36cc6e55c47157b1
Nikhil Verma <vermanick75@gmail.com>
2024-07-25 18:15:58 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 import About from './pages/About'
 function App() {
   return (
     <BrowserRouter>
+    <Header />
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
